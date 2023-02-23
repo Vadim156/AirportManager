7902d4af671d1f532b04e5a16edad979b27170b5
Vadim Vologin <106904639+Vadim156@users.noreply.github.com>
2023-02-23 06:15:56 +0200

    Delete Airport.Data directory

--- Airport.Data/Repositories/ITerminalRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using FinalProj.Models;
-using Logic.Repositories;
-
-namespace Airport.Data.Repositories
-{
-    public interface ITerminalRepository : IGenericRepository<Terminal>
-    {
-    }
-}
